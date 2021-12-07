be416ab68d1fb9a69d40238432c10d3bf4e88295
temirlan <temirlan.zarymkanov@nu.edu.kz>
2021-12-07 10:45:24 +0600

    hotels 1.0

--- src/main/java/model/Hotel.java
+++ src/main/java/model/Hotel.java
@@ -1,6 +1,155 @@
 package model;
 
+import dao.GenericDAO;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Entity
 public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer hotel_id;
+    private String city;
+    private String name;
+    private String address;
+    private static GenericDAO DAO = new GenericDAO(Hotel.class);
+
+    public Hotel(String city, String name, String address){
+        this.city = city;
+        this.name = name;
+        this.address = address;
+    }
+    public Hotel(){
+
+    }
+    public Hotel(String[] data){
+        for(String x : data){
+            String[] add = x.split("=");
+            if(add.length == 1){
+                continue;
+            }
+            if(add[0].equals("name")){
+                this.name = add[1];
+            }
+            if(add[0].equals("city")){
+                this.city = add[1].replaceAll("[\\n\\t]", "");
+            }
+
+            if(add[0].equals("address")){
+                this.address = add[1];
+            }
+        }
+    }
+    public Hotel(HttpServletRequest request){
+        if(request.getParameter("name").isEmpty()){
+            this.name = null;
+        }else{
+            this.name = request.getParameter("name");
+        }
+
+        if(request.getParameter("city").isEmpty()){
+            this.city = null;
+        }else{
+            this.city = request.getParameter("city");
+        }
+
+        if(request.getParameter("address").isEmpty()){
+            this.address = null;
+        }else{
+            this.address = request.getParameter("address");
+        }
+    }
+    public Integer getId() {
+        return hotel_id;
+    }
+
+    public Hotel setId(Integer hotel_id) {
+        this.hotel_id = hotel_id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hotel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Hotel setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public Hotel setCity(String city) {
+        this.city = city;
+        return this;
+    }
+
+    public static Hotel save(Hotel hotel){
+        return (Hotel) DAO.save(hotel);
+    }
+
+    public static Hotel find(Integer id){
+        return (Hotel) DAO.find(id);
+    }
+
+    public static List<Hotel> findAll(){
+        return DAO.findAll();
+    }
+
+    public static List<Hotel> findAll(String args){
+        return DAO.findAll(args);
+    }
+
+    public static void update(Hotel hotel){
+        DAO.update(hotel);
+    }
+
+    public static void delete(Integer id){
+        DAO.delete(id);
+    }
+
+    public Hotel save(){
+        return (Hotel) DAO.save(this);
+    }
+
+    public void update(){
+        DAO.update(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Hotel{" +
+                "hotel_id=" + hotel_id +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", address=" + address +
+                '}';
+    }
+
+    public String toJSON(){
+        return "{" +
+                "\"hotel_id\":\"" + hotel_id + "\""+
+                ", \"name\":\"" + name +"\""+
+                ", \"city\":\"" + city +"\""+
+                ", \"address\":\"" + address + "\""+
+                "}";
+    }
 
     public static String calculateSituation(
             Double occupiedRoomsPercentage,
---
+++ web/WEB-INF/hotel/findAll.jsp
@@ -0,0 +1,45 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/find.css">
+    <title>Hotels</title>
+    <meta name="viewport" content="width=device-width, user-scalable=0">
+</head>
+<body>
+<c:import url="/WEB-INF/header/main.jsp"/>
+<div class="content">
+    <h1>Hotels</h1>
+    <div class="over">
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            <tbody>
+            <c:forEach items="${hotelList}" var="hotel">
+                <tr onclick="window.location.href='/hotels/${hotel.getId()}';">
+                    <td>${hotel.getName()}</td>
+                    <td>${hotel.getCity()}</td>
+                    <td>$${hotel.getAddress()}</td>
+                    <td class="count"> - </td></tr>
+            </c:forEach>
+
+            </tbody>
+        </table>
+    </div>
+
+    </table>
+    <c:if test="${allowed == true}">
+    <div class="submit">
+        <button onclick="window.location.href='/hotels/new';">New Hotel</button>
+    </div>
+    </c:if>
+</div>
+
+</body>
+</html>
