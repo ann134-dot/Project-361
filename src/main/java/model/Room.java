2cb7c117cb240d8ae3e968369eb44e2a14f87a21
Ann <anara.zhangbyrshyn@nu.edu.kz>
2021-12-07 12:08:05 +0600

    Added TempStign2

--- src/main/java/model/Room.java
+++ src/main/java/model/Room.java
@@ -16,6 +16,8 @@ public class Room {
     private RoomType roomType;
     private String TempString;
 
+    private String TempString2;
+
     private Integer number;
     private static GenericDAO DAO = new GenericDAO(Room.class);
 
